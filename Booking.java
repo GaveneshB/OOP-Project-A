eca7895c4562abf62a213d025e85a9b006a2d76a
MUHAMMAD ALIFF BIN HELMI <aliffhelmi05@gmail.com>
2026-05-18 12:13:26 +0800

    Create Booking.java

---
+++ Booking.java
@@ -0,0 +1,13 @@
+public class Booking {
+
+    enum FacilityType { 
+        STUDY_ROOM, COMPUTER_LAB, SPORT_COURT, SEMINAR_ROOM 
+} 
+    enum TimeSlot { 
+        MORNING, AFTERNOON, EVENING 
+} 
+    enum BookingStatus { 
+        PENDING, APPROVED, REJECTED, CANCELLED 
+} 
+
+}
