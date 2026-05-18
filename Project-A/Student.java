07a63e52e362920d0dae67f639b6740ee6a2132d
MUHAMMAD ALIFF BIN HELMI <aliffhelmi05@gmail.com>
2026-05-18 11:11:05 +0800

    Create Student.java

---
+++ Project-A/Student.java
@@ -0,0 +1,84 @@
+
+public class Student {
+    private String student_ID;
+    private String student_Name;
+    private String programme;
+    private int year_Of_Study;
+    private StudentCategory category;
+
+    enum StudentCategory {
+        UNDERGRADUATE,
+        POSTGRADUATE,
+        EXCHANGE
+    }
+
+    public Student() {
+        this.student_ID = "";
+        this.student_Name = "";
+        this.programme = "";
+        this.year_Of_Study = 0;
+        this.category = StudentCategory.UNDERGRADUATE;
+    }
+
+    public Student(String student_ID, String student_Name, String programme, int year_Of_Study, StudentCategory category) {
+        this.student_ID = student_ID;
+        this.student_Name = student_Name;
+        this.programme = programme;
+        this.year_Of_Study = year_Of_Study;
+        this.category = category;
+    }
+
+    public String getStudent_ID() {
+        return student_ID;
+    }
+
+    public void setStudent_ID(String student_ID) {
+        this.student_ID = student_ID;
+    }
+
+    public String getStudent_Name() {
+        return student_Name;
+    }
+
+    public void setStudent_Name(String student_Name) {
+        this.student_Name = student_Name;
+    }
+
+    public String getProgramme() {
+        return programme;
+    }
+
+    public void setProgramme(String programme) {
+        this.programme = programme;
+    }
+
+    public int getYear_Of_Study() {
+        return year_Of_Study;
+    }
+
+    public void setYear_Of_Study(int year_Of_Study) {
+        this.year_Of_Study = year_Of_Study;
+    }
+
+    public StudentCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(StudentCategory category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "student_ID='" + student_ID + '\'' +
+                ", student_Name='" + student_Name + '\'' +
+                ", programme='" + programme + '\'' +
+                ", year_Of_Study=" + year_Of_Study +
+                ", category=" + category +
+                '}';
+    }
+
+    
+
+}
